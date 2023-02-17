68f40f3a7ccf7691307ffb9247200a2acbbc244f
Evellyn Machado <100969004+EveMachado@users.noreply.github.com>
2023-02-17 13:02:26 -0300

    função do programa "Triangulo".

---
+++ funçãoTriangulo.java
@@ -0,0 +1,12 @@
+public class triangulo {
+
+    public double a;
+    public double b;
+    public double c;
+
+    public double area() {
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+}
