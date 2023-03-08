571d19db14fabb62dd31dbd06f938307451f85a5
Evellyn Machado <100969004+EveMachado@users.noreply.github.com>
2023-03-08 16:00:27 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,38 +1,42 @@
 import java.util.Scanner;
 
-public class Produto {
+public class programaProduto {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        Produto produto = new Produto();
-        System.out.println("Insira dados do produto: ");
-        System.out.print("Nome: ");
-        produto.nome = sc.nextLine();
-        System.out.print("preço: ");
-        produto.preco = sc.nextDouble();
-        System.out.print("estoque: ");
-        produto.estoque = sc.nextInt();
+        boolean continuar = true;
 
 
-        System.out.println();
-        System.out.println("dados dos produtos: " + produto);
+        while (continuar) {
+            Produto produto = new Produto();
 
-        System.out.println();
-        System.out.print("entre com o número de produtos para adicionar no estoque: ");
-        int estoque = sc.nextInt();
-        produto.addProdutos(estoque);
 
-        System.out.println();
-        System.out.println("dados novos: " + produto);
+            System.out.println();
+            System.out.println("dados dos produtos: " + produto);
 
-        System.out.println();
-        System.out.print("entre com o número de produtos para tirar do estoque: ");
-        estoque = sc.nextInt();
-        produto.removerProduto(estoque);
+            System.out.println();
+            System.out.print("entre com o número de produtos para adicionar no estoque: ");
+            int estoque = sc.nextInt();
+            produto.addProdutos(estoque);
 
-        System.out.println();
-        System.out.println("dados novos: " + produto);
+            System.out.println();
+            System.out.println("dados novos: " + produto);
+
+            System.out.println();
+            System.out.print("entre com o número de produtos para tirar do estoque: ");
+            estoque = sc.nextInt();
+            produto.removerProduto(estoque);
+
+            System.out.println();
+            System.out.println("dados novos: " + produto);
+
+            System.out.println("deseja ver outro produto? (s/n)");
+            char resposta = sc.next().charAt(0);
+
+            if(resposta == 'n' || resposta == 'N') {
+                continuar = false;
+            }
+        }
     }
 }
